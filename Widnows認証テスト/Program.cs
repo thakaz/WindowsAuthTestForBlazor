a69916f4566175fba1d81da9de7098539091e2c7
n-miura <n-miura@s-crest.co.jp>
2024-06-21 15:22:05 +0900

    Revert "Typo"
    This reverts commit 0a7c051ad0ea85637abb66110d9f12823e81fc45.

--- Widnows認証テスト/Program.cs
+++ Widnows認証テスト/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
