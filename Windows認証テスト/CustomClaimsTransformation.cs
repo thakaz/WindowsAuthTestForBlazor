3383b0148d99c738b5a16050cd3353fa355b5005
n-miura <n-miura@s-crest.co.jp>
2024-06-22 03:06:45 +0900

    独自認証の追加

---
+++ Windows認証テスト/CustomClaimsTransformation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Security.Claims;
+using Windows認証テスト.DB;
+
+namespace Windows認証テスト
+{
+    public class CustomClaimsTransformation : IClaimsTransformation
+    {
+        private readonly IDbContextFactory<TestDBContext> _dbFactory;
+
+        public CustomClaimsTransformation(IDbContextFactory<TestDBContext> dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var identity = (ClaimsIdentity)principal.Identity;
+
+            var newIdentity = new ClaimsIdentity(identity.Claims, identity.AuthenticationType);
+
+            //DBから取得したロール情報を追加
+            using (var db = _dbFactory.CreateDbContext())
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserId == identity.Name);
+
+                if (user != null)
+                {
+                    //とりあえず1ならAdmin、それ以外はUserとする
+                   // identity.AddClaim(new Claim(ClaimTypes.Role, user.Role == 1 ? "Admin" : "User"));
+                    newIdentity.AddClaim(new Claim(ClaimTypes.Role, user.Role == 1 ? "Admin" : "User"));
+                }
+            }
+
+            var newPrincipal = new ClaimsPrincipal(newIdentity);
+            principal.AddIdentity(newIdentity);
+
+            return Task.FromResult(principal);
+        }
+    }
+}
---
+++ Windows認証テスト/Migrations/20240621085251_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Windows認証テスト.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "text", nullable: false, comment: "ユーザーID"),
+                    role = table.Column<int>(type: "integer", nullable: false, comment: "権限コード")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                },
+                comment: "ログイン管理用");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
