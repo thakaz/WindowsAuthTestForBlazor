be8b977f198e2ba1a602f87a34f7b8aa3b91bc14
n-miura <n-miura@s-crest.co.jp>
2024-06-21 16:04:31 +0900

    typo

---
+++ Windows認証テスト/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Windows認証テスト.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+        options.FallbackPolicy = options.DefaultPolicy;
+});
+
+
+builder.Services.AddHttpContextAccessor();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
