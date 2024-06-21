0a7c051ad0ea85637abb66110d9f12823e81fc45
n-miura <n-miura@s-crest.co.jp>
2024-06-21 15:03:42 +0900

    Typo

--- Windows認証テスト/Program.cs
+++ Windows認証テスト/Program.cs
@@ -18,8 +18,6 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
